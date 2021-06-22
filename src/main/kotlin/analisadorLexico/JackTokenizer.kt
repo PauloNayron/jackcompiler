ed5c2af32da6f06d4eeb9c0552b42b2de41488a1
PauloNayron <paulonayron.s@hotmail.com>
2021-06-21 21:16:19 -0300

    fix: adjust git

---
+++ src/main/kotlin/analisadorLexico/JackTokenizer.kt
@@ -0,0 +1,5 @@
+package analisadorLexico
+
+interface JackTokenizer {
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+import java.io.File
+
+fun readFileLineByLine (fileName: String) : Collection<String> = File(fileName).readLines().toList()
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val lines = readFileLineByLine("./src/main/resources/Main.jack")
+    println("<token>")
+    lines.forEach {
+
+    }
+    println("</token>")
 }
