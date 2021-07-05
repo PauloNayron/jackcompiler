2567cb34008c8df05d0fd1abab95b39db8940665
PauloNayron <paulonayron.s@hotmail.com>
2021-07-04 22:34:46 -0300

    refactor: adiciona regra para ler linha a linha

--- src/main/kotlin/analisadorLexico/impl/JackTokenizerImpl.kt
+++ src/main/kotlin/analisadorLexico/impl/JackTokenizerImpl.kt
@@ -3,17 +3,30 @@ package analisadorLexico.impl
 import analisadorLexico.JackTokenizer
 import analisadorLexico.Token
 import analisadorLexico.enums.TokenType
+import java.lang.NullPointerException
 
 data class JackTokenizerImpl(
-    var line: String,
+    var line: String = "",
+    var count: Int = 1,
+    var mapLines: Map<Int, String>?,
     var currentToken: String? = null
 ): JackTokenizer {
-    constructor(line: String) : this (
-        line = line.split("//")[0],
+    constructor(src: String) : this (
+        mapLines = exec(src),
         currentToken = null
     )
 
-    override fun hasMoreTokens(): Boolean = REGEX.containsMatchIn(this.line)
+    override fun hasMoreTokens(): Boolean {
+        try {
+            while (this.line.isBlank()) {
+                this.line = this.mapLines?.get(this.count)!!
+                this.count += 1
+            }
+            return REGEX.containsMatchIn(this.line)
+        } catch (e: NullPointerException) {
+            return false
+        }
+    }
 
     override fun advance(): Token {
         if (this.hasMoreTokens()) {
@@ -31,6 +44,35 @@ data class JackTokenizerImpl(
 
     companion object {
         val REGEX = "[0-9][0-9]*|[\\[]|[\\]]|[aA-zZ][aA-zZ]*|[aA-zZ_][aA0-zZ9_]*|[\"][aA-zZ_][aA0-zZ9_]*[ ]*[aA0-zZ9_]*[\"]|[(]|[)]|[<]|[>]|[=]|[{]|[}]|[\\/]{2}[/]*|[;]|[.]|[,]|[\\|]|[\\-]|[*]|[+]|[&]".toRegex()
+
+        fun exec (src: String): Map<Int, String> {
+            val lines = src.split("\n")
+
+            val mapLine = HashMap<Int, String>()
+            var count = 1
+            var linhaComentada = false
+            for (line in lines) {
+                if (linhaComentada && !line.contains("*/")) {
+                    mapLine.put(count++, "")
+                } else if (line.contains("/*")) {
+                    val split = line.split("/*")
+                    if (split[1].contains("*/")) {
+                        linhaComentada = false
+                        val split2 = split[1].split("*/")[1]
+                        mapLine.put(count++, "${split[0]}${split2}")
+                    } else {
+                        linhaComentada = true
+                        mapLine.put(count++, split[0])
+                    }
+                } else if (line.contains("*/")) {
+                    linhaComentada = false
+                    mapLine.put(count++, line.split("*/")[1])
+                } else {
+                    mapLine.put(count++, line.split("//")[0])
+                }
+            }
+            return mapLine
+        }
     }
 }
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,15 +3,15 @@ import java.io.File
 
 fun readFileLineByLine (fileName: String) : Collection<String> = File(fileName).readLines().toList()
 
-fun main(args: Array<String>) {
-    val lines = readFileLineByLine("./src/main/resources/Square.jack")
+fun readFile (fileName: String) : String = File(fileName).readText()
 
+fun main(args: Array<String>) {
     println("<tokens>")
-    lines.forEach{ line ->
-        val jackTokenizer = JackTokenizerImpl(line)
-        while (jackTokenizer.hasMoreTokens()) {
-            println(jackTokenizer.advance())
-        }
+    val src = readFile("./src/main/resources/Square.jack")
+    val jackTokenizer = JackTokenizerImpl(src)
+
+    while (jackTokenizer.hasMoreTokens()) {
+        println(jackTokenizer.advance())
     }
     println("</tokens>")
 }
